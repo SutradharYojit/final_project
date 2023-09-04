07509e240bfb5165a57a3f8926cf67b762b6b1ae
SutradharYojit <jimmysuthar08@gmail.com>
2023-09-04 11:30:07 +0530

    initial commit

---
+++ lib/app/app.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../view/view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        );
+      },
+    );
+  }
+}
---
+++ lib/view/view.dart
@@ -0,0 +1,4 @@
+export 'splash/splash_screen.dart';
+export 'auth/login/login_screen.dart';
+export 'auth/signup/signup_screen.dart';
+export 'home/home_screen.dart';
